7ff6a92624fecd9c8fbfad35cf0588b83e920783
Anggi Priatmadi <anggi.arlandi@gmail.com>
2016-06-17 23:55:51 -0400

    working server

--- server.js
+++ server.js
@@ -1,33 +1,19 @@
-'use strict';
+var express = require('express');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
 
-var express = require('express'),
-    bodyParser = require('body-parser'),
-    morgan = require('morgan'),
-    app = express();
 
-var bcrypt = require('bcrypt');
-const saltRounds = 10;
-const myPlaintextPassword = 's0/\/\P4$$w0rD';
-const someOtherPlaintextPassword = 'bacon_tastes_good';
+var routes = require('./routes/users.js');
 
-/*
- keep this for now till ready to save in DB;
- bcrypt.genSalt(saltRounds, function(err, salt) {
- bcrypt.hash(myPlaintextPassword, salt, function(err, hash) {
+// *** express instance *** //
+var app = express();
 
- });
+// *** config middleware *** //
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
- // Load hash from your password DB.
- bcrypt.compare(myPlaintextPassword, hash, function(err, res) {
- // res == true
- });
- bcrypt.compare(someOtherPlaintextPassword, hash, function(err, res) {
- // res == false
- });
- });*/
-
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
+// *** main routes *** //
+app.use('/', routes);
 
 module.exports = app;
