b934b45b22a1e7d4d20c2053105718cce3e826fc
chriskimm1 <kchris81@gmail.com>
2016-06-17 23:29:20 -0400

    created server.js and bin

---
+++ server.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    morgan = require('morgan'),
+    app = express();
+
+var bcrypt = require('bcrypt');
+const saltRounds = 10;
+const myPlaintextPassword = 's0/\/\P4$$w0rD';
+const someOtherPlaintextPassword = 'bacon_tastes_good';
+
+/*
+ keep this for now till ready to save in DB;
+ bcrypt.genSalt(saltRounds, function(err, salt) {
+ bcrypt.hash(myPlaintextPassword, salt, function(err, hash) {
+
+ });
+
+ // Load hash from your password DB.
+ bcrypt.compare(myPlaintextPassword, hash, function(err, res) {
+ // res == true
+ });
+ bcrypt.compare(someOtherPlaintextPassword, hash, function(err, res) {
+ // res == false
+ });
+ });*/
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(3322, function () {
+    console.log('Server running on port 3322');
+});
